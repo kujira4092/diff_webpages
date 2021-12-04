ee2f8f43e4fe4231e232f21254c37399c5afaa22
nagi4092 <trbm.tumiki.4092@gmail.com>
2021-12-04 15:00:54 +0900

    sync capture

--- modules/screenshot.js
+++ modules/screenshot.js
@@ -1,6 +1,8 @@
 'use strict';
 
-exports.screenshot = (async (device='pc',baseUrlArr=['https://www.smbc-card.com/nyukai/card/numberless.jsp','https://www.smbc-card.com/nyukai/card/gold-numberless.jsp'],compareUrlArr=['https://www.smbc-card.com/nyukai/card/numberless.jsp','https://www.smbc-card.com/nyukai/card/gold-numberless.jsp'])=>{
+const { SSL_OP_NETSCAPE_CA_DN_BUG } = require('constants');
+
+exports.screenshot = (async (device='pc',urlArr=['https://www.smbc-card.com/nyukai/card/numberless.jsp','https://www.smbc-card.com/nyukai/card/gold-numberless.jsp'],testDomain="https://www.smbc-card.com/")=>{
     const puppeteer = require('puppeteer');
     const fs = require('fs');
     require('date-utils');
@@ -32,72 +34,91 @@ exports.screenshot = (async (device='pc',baseUrlArr=['https://www.smbc-card.com/
             //console.log(`ScrollHeight ${scrollHeight}`);
         }
     }
-    return (async ()=>{
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
 
-        //width setup
-        let _width = 0;
-        if(device == 'pc'){
-            _width = 1200;
-        }
-        else if(device == 'sp'){
-            _width = 375;
-        }
-        let _height = 800;
-        await page.setViewport({
-            width: _width,
-            height: _height
-        });
+    //check
+    let chklist = [];
+    urlArr.forEach(url=>{
+        let _obj = new Object();
+        _obj.base = url;
+        _obj.compare = url.replace(/^(((https)|(http)):\/\/.*?)\//g,testDomain);
+        _obj.filename = url.replace('.jsp','').replace(/^(((https)|(http)):\/\/.*?)\//g,'').replace(/\//g,'_');
+        chklist.push(_obj);
+    });
 
-        var type = 'base';
+    console.log(chklist);
 
-        for(let i=0;i<baseUrlArr.length;i++){
-            let url = baseUrlArr[i];
-            await page.goto(url);
-            console.log(`Loading page ${url} ...`);
-            //networkidle2 : connection<2,time<500ms
-            await page.waitForNavigation({waitUntil:'load', timeout: 5000}).catch(e => {});
-            let dir = `./result/${currentTime}/${type}/screenshot/`;
-            if(url.match(/.$/) == '/'){url = url+'index';}
-            let filename = `${url.replace('.jsp','').replace(/^(((https)|(http)):\/\/.*?)\//g,'').replace(/\//g,'-')}`;
+    let promiseList = [];
+    //const browser = await puppeteer.launch();
+    for(var i=0;i<chklist.length;i++){
+        let obj = chklist[i];
+        promiseList.push((async ()=>{
+            const browser = await puppeteer.launch();
+            const base = await browser.newPage();
+    
+            //width setup
+            let _width = 0;
+            if(device == 'pc'){
+                _width = 1200;
+            }
+            else if(device == 'sp'){
+                _width = 375;
+            }
+            let _height = 800;
+            await base.setViewport({
+                width: _width,
+                height: _height
+            });
+    
+            let type='base';
+            let filepath='',dir='',filename='';
+            
+            filepath = obj[type];
+            await base.goto(filepath);
+            console.log(`Loading page ${filepath} ...`);
+            await base.waitForNavigation({waitUntil:'load', timeout: 7500}).catch(e => {console.log('timeout');});
+            dir = `./result/${currentTime}/${type}/screenshot/`;
+            if(filepath.match(/.$/) == '/'){filepath = filepath+'index';}
+            filename = `${filepath.replace('.jsp','').replace(/^(((https)|(http)):\/\/.*?)\//g,'').replace(/\//g,'-')}`;
             if (!fs.existsSync(dir)) {
                 fs.mkdirSync(dir, { recursive: true });
             }
-            await scrollToBottom(page,_height);
-            await page.screenshot({
+            await scrollToBottom(base,_height);
+            await base.screenshot({
                 path:`${dir}${filename}.png`,
                 fullPage: true
             });
-            console.log(`Complete!`);
-        }
 
-        type = 'compare';
-        //const authdata = JSON.stringify(fs.readFileSync('./auth.json'));
+            const compare = await browser.newPage();
+            await compare.bringToFront();
+            await compare.setViewport({
+                width: _width,
+                height: _height
+            });
+            type = 'compare';
 
-        for(let i=0;i<compareUrlArr.length;i++){
-            let url = compareUrlArr[i];
-            console.log(`Loading page ${url} ...`);
-            await page.authenticate({username: 'username',password: 'password'});
-            await page.goto(url);
-            //networkidle2 : connection<2,time<500ms
-            await page.waitForNavigation({waitUntil:'load', timeout: 5000}).catch(e => {});
-            let dir = `./result/${currentTime}/${type}/screenshot/`;
-            if(url.match(/.$/) == '/'){url = url+'index';}
-            let filename = `${url.replace('.jsp','').replace(/^(((https)|(http)):\/\/.*?)\//g,'').replace(/\//g,'-')}`;
+            filepath = obj[type];
+            await compare.goto(filepath);
+            console.log(`Loading page ${filepath} ...`);
+            await compare.waitForNavigation({waitUntil:'load', timeout: 7500}).catch(e => {console.log('timeout');});
+            dir = `./result/${currentTime}/${type}/screenshot/`;
+            if(filepath.match(/.$/) == '/'){filepath = filepath+'index';}
+            filename = `${filepath.replace('.jsp','').replace(/^(((https)|(http)):\/\/.*?)\//g,'').replace(/\//g,'-')}`;
             if (!fs.existsSync(dir)) {
                 fs.mkdirSync(dir, { recursive: true });
             }
-            await scrollToBottom(page,_height);
-            await page.screenshot({
+            await scrollToBottom(compare,_height);
+            await compare.screenshot({
                 path:`${dir}${filename}.png`,
                 fullPage: true
             });
-            console.log(`Complete!`);
-        }
-        await browser.close();
-        console.log(`Capture completed!`);
+            await browser.close();
 
-        return currentTime;
-    })();
+            return true;
+        })());
+    };
+
+    //console.log(promiseList);
+    await Promise.all(promiseList);
+    console.log(`Capture completed!`);
+    return currentTime;
 });
--- test.js
+++ test.js
@@ -2,7 +2,6 @@ const test = require('./modules/screenshot.js');
 const createHTML = require('./modules/create-html');
 
 test.screenshot().then((data)=>{
+    console.log(data);
     return createHTML.createHTML(`./result/${data}/`);
-}).then((res) => { 
-    console.log(1); 
 });
